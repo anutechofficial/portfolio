c0a9989702279ccc8ee5c2a2db471cec49f207e6
Anurag Yadav <anutechofficial@gmail.com>
2024-08-29 18:31:40 +0530

    changes in discriptions and added Youtube embed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // ============== typing animation
 
 var typed = new Typed(".typing", {
-    strings:[" ", "MERN Stack", "Web Developer", "Tech YouTuber" ],
+    strings:[" ", "Backend Engineer", "Web Developer", "Tech Content Creator" ],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
